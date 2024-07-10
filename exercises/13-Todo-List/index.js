06e979662baa6c258df6b23ca66f90540b5897e8
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-07-10 10:13:03 +0000

    Me encanta esta sh...

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,19 @@
 // Your code here
+let addTask = function() {
+    let selPadreAdd = document.querySelector("ul");
+    let createHtmlElement = document.createElement("li");
+    let selInput = document.getElementById("addToDo").value;
+    createHtmlElement.innerHTML = `<span><i class="fa fa-trash"></i></span> ${selInput}`;
+    selPadreAdd.appendChild(createHtmlElement);
+    document.getElementById("addToDo").value = "";
+}
+
+document.getElementById("addToDo").addEventListener('keypress', function(event){
+    if (event.key === 'Enter'){addTask();}
+});
+
+document.querySelector("ul").addEventListener('click', function(event){
+    if (event.target.classList.contains('fa-trash')){
+        event.target.parentElement.parentElement.remove();
+    }
+});
